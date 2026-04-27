1833efc9ec7e5b5429d8c42aea7c78fe5e27f50d
2DimBer3 <berezan4@mail.ru>
2026-04-27 21:01:42 +0300

    fix step 5

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -6,8 +6,6 @@ import picocli.CommandLine;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.Map;
 
 @CommandLine.Command(name = "gendiff", mixinStandardHelpOptions = true,
